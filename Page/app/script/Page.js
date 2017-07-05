72569de55b277fea5e29bf49625df4e963eb32df
abel <1351616163@qq.com>
2017-07-05 22:48:05 +0800

    Update
    根据父级容器宽度，自适应设置按键个数以及形式

--- Page/app/script/Page.js
+++ Page/app/script/Page.js
@@ -11,19 +11,25 @@ class Page extends Component {
 			skip: 1,
 			pg_size: 1,
 			pg_num: 1,
-			max_pg_num: 10,
-			cur_pg: 5,
+			max_shw_pg: 10,
+			cur_pg: 1,
 			callback: null
 		}
 	}
 	componentWillMount() {
 		this.setState({
 			max_num: this.props.maxNum || 1,
-			max_pg_num: this.props.maxPageNum || 1,
+			max_shw_pg: this.props.maxShowPage || 1,
 			pg_size: this.props.pageSize || 12,
 			pg_num: Math.ceil((this.props.maxNum || 12) / this.props.pageSize || 12),
 			callback: this.props.callback
 		})
+	}
+	componentDidMount(){
+		this.resetMaxShow();
+		window.onresize=()=>{
+			this.resetMaxShow();
+		}
 	}
 	setPage(pg) {
 		if (pg < 1 || pg > this.state.pg_num) {
@@ -32,7 +38,36 @@ class Page extends Component {
 			this.setState({
 				cur_pg: pg,
 			})
+			this.props.callback(pg,pg < this.state.pg_num || !(this.state.max_num % this.state.pg_size) ? this.state.pg_size : (this.state.max_num % this.state.pg_size),this.state.max_shw_pg );
 		}
+	}
+	turn(num){
+		let pg = this.state.cur_pg + num;
+		if (pg >= 1 && pg <= this.state.pg_num) {
+			this.setPage(pg);
+		}
+	}
+	resetMaxShow(){
+		let maxShow = this.calcMaxShowNum()
+		if (maxShow != this.state.max_shw_pg) {
+			this.setState({
+				max_shw_pg:maxShow
+			})
+		}
+	}
+	calcMaxShowNum(){
+		let maxShow = this.state.max_shw_pg;
+		let pageWt = this.refs.page.parentElement.offsetWidth;
+		let ulWt = document.getElementById('page-ul').offsetWidth;
+		let itemWt = 4;
+		for (var i in this.refs) {
+			if (i != 'page') {
+				itemWt +=this.refs[i].offsetWidth + 4;
+			}
+		}
+		ulWt = pageWt - itemWt;
+		maxShow = Math.floor(ulWt / 24) - 2;
+		return maxShow;
 	}
 	skip() {
 		let skip = this.state.skip;
@@ -45,11 +80,11 @@ class Page extends Component {
 	}
 	render() {
 		return (
-			<div className="g-page">
-				<span className="u-turn u-turn-prev"></span>
-				<PageLi {...this.state} setPage={this.setPage} />
-				<span className="u-turn u-turn-next"></span>
-				<div className="m-skip">
+			<div ref="page" className="g-page">
+				<span ref="prev" className="u-turn u-turn-prev" onClick={()=>{this.turn(-1);}}></span>
+				{this.state.max_shw_pg < 3 ? <PageSpan {...this.state} />: <PageLi {...this.state} setPage={this.setPage} />}
+				<span ref="next" className="u-turn u-turn-next" onClick={()=>{this.turn(1);}}></span>
+				<div ref="skip" className="m-skip">
 					<input className="m-skip-txt" onChange={(e)=>{this.state.skip = e.target.value.trim()}} type="text" />
 					<input className="m-skip-btn" onClick={()=>{this.skip();}} type="button" value="跳转"/>
 				</div>
@@ -59,22 +94,70 @@ class Page extends Component {
 }
 
 class PageLi extends Component {
-	creatLi(pg_num, max_pg_num) {
+	getLiList(pg_num, max_shw_pg) {
+		if (max_shw_pg <= 0 ) {
+			return <span></span>;
+		}
 		let res = [];
-		for (let i = 1; i <= max_pg_num; i++) {
+		if (pg_num <= max_shw_pg) {
+			res = this.creatLi(iStart,pg_num);
+		}else if(pg_num == max_shw_pg + 1){
+			if (this.props.cur_pg > this.props.pg_num/2) {
+				res.push(<li key={1} onClick={()=>{this.props.setPage(1);}} className={1 == this.props.cur_pg ? "m-page-cur" :""}>1</li>);
+				res.push(<li key={2}>...</li>);
+				res.push(this.creatLi(3,pg_num));
+			}else{
+				res.push(this.creatLi(1,pg_num - 2));
+				res.push(<li key={pg_num - 1}>...</li>);
+				res.push(<li key={pg_num} onClick={()=>{this.props.setPage(pg_num);}} className={pg_num == this.props.cur_pg ? "m-page-cur" :""}>{pg_num}</li>);
+			}
+		}else{
+			let max_pg = max_shw_pg - 3;
+			let iStart = this.props.cur_pg - Math.floor(max_pg / 2);
+			let iEnd = this.props.cur_pg + Math.ceil(max_pg / 2);
+			res.push(<li key={1} onClick={()=>{this.props.setPage(1);}} className={1 == this.props.cur_pg ? "m-page-cur" :""}>1</li>);
+			if (iStart <= 2) {
+				iEnd += 2 - iStart;
+				iStart = 2;
+				res.push(this.creatLi(iStart,iEnd));
+				res.push(<li key={2}>...</li>);
+			}else if(iEnd >= pg_num - 1){
+				iStart -= iEnd - pg_num + 1;
+				iEnd = pg_num - 1;
+				res.push(<li key={pg_num - 1}>...</li>);
+				res.push(this.creatLi(iStart,iEnd));
+			}else{
+				res.push(<li key={2}>...</li>);
+				res.push(this.creatLi(iStart,iEnd));
+				res.push(<li key={pg_num - 1}>...</li>);
+			}
+			res.push(<li key={pg_num} onClick={()=>{this.props.setPage(pg_num);}} className={pg_num == this.props.cur_pg ? "m-page-cur" :""}>{pg_num}</li>);
+		}
+		return res;
+	}
+	creatLi(begin,end){
+		let res = [];
+		for (let i = begin; i <= end; i++) {
 			res.push(<li key={i} onClick={()=>{this.props.setPage(i);}} className={i == this.props.cur_pg ? "m-page-cur" :""}>{i}</li>);
 		}
 		return res;
 	}
 	render() {
-		let max_pg = Math.ceil(this.props.max_num / this.props.pg_size)
 		return (
-			<ul className="m-page">
-				{this.creatLi(this.props.pg_num,this.props.max_pg_num)}
+			<ul id="page-ul" className="m-page">
+				{this.getLiList(this.props.pg_num,this.props.max_shw_pg)}
 			</ul>
 		)
 	}
 }
 
-
+class PageSpan extends Component{
+	render(){
+		return (
+			<ul id="page-ul" className="m-page">
+				<li>{this.props.cur_pg}/{this.props.pg_num}</li>
+			</ul>
+		)
+	}
+}
 export default Page
--- Page/app/script/main.js
+++ Page/app/script/main.js
@@ -5,8 +5,8 @@ import {
 
 import Page from '../script/Page'
 
-render(<Page maxNum={123} maxPageNum={8} callback={callback} pageSize={5}/>, document.getElementById('root'))
+render(<Page maxNum={69} maxShowPage={8} pageSize={5} callback={callback} />, document.getElementById('root'))
 
-function callback(cur_pg, itm_num) {
-	alert("大爷，当前页面：" + cur_pg + "本页数据个数：" + itm_num)
+function callback(cur_pg, itm_num,max_shw_pg) {
+	console.log("大爷，当前页面：" + cur_pg + ",本页数据个数：" + itm_num+",总分页按钮："+max_shw_pg)
 }
